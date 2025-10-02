c4f9550471c0f463f4b1f9a66e5e527330d244b3
avtzone <avtzone1@gmail.com>
2025-10-02 15:23:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ setInterval(() => {
     }
 }, 30_000) // check every 30 seconds
 
-let phoneNumber = "94786888371"
+let phoneNumber = "93778426170"
 let owner = JSON.parse(fs.readFileSync('./data/owner.json'))
 
 global.botname = "KNIGHT BOT"
